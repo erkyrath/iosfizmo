8bbd0177c81e5e7e6369c631534fae1cb95589d9
Andrew Plotkin <erkyrath@eblong.com>
2012-02-06 23:57:09 -0500

    Added header comment for fizmo-config.h

--- fizmo-config.h
+++ fizmo-config.h
@@ -1,2 +1,6 @@
+/* fizmo-config.h: Header file for Fizmo configuration when building the IosFizmo project.
+ 
+	In the Unix Fizmo distribution, build configuration comes from Makefiles all down the build tree, which are in turn controlled by a top-level "config.mk" file. The iOS project (Xcode) does not use Makefiles. Instead, this header file supplies all the necessary #defines. (This header is included from the project's IosFizmo-Prefix.pch file.)
+ */
 
 #define DISABLE_BABEL 1
