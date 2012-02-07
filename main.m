d68e85159494d8843127f1536bc4d433a7700b03
Andrew Plotkin <erkyrath@eblong.com>
2012-02-06 22:04:59 -0500

    First checkin with project file

--- main.m
+++ main.m
@@ -1,17 +1,35 @@
 /* main.m: Top main() function
-	for Glulxe, the Glulx VM interpreter.
-	Designed by Andrew Plotkin <erkyrath@eblong.com>
-	http://eblong.com/zarf/glulx/
+	for IosFizmo, an IosGlk port of the Fizmo Z-machine interpreter.
 */
 
 /*
-The source code in this package is copyright 2011 by Andrew Plotkin. You
-may copy and distribute it freely, by any means and under any conditions,
-as long as the code and documentation is not changed. You may also
-incorporate this code into your own program and distribute that, or modify
-this code and use and distribute the modified version, as long as you retain
-a notice in your program or documentation which mentions my name and the
-URL shown above.
+ Fizmo:
+ Copyright (c) 2009-2011 Christoph Ender
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 1. Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 3. The name of the author may not be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ For more information, see fizmo/COPYRIGHT.txt
 */
 
 #import <UIKit/UIKit.h>
